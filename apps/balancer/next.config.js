250301438d178853eb5394934ecf10ff4877e40d
groninge <groninge@gmail.com>
2024-09-05 14:18:13 +0200

    not needed

--- apps/balancer/next.config.js
+++ apps/balancer/next.config.js
@@ -16,7 +16,6 @@ const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
     return config
   },
   logging: {
